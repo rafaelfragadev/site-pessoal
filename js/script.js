370a7110c71608748973d47c36b0af18b75d0da9
Rafael Fraga <rafaelfragawebdesigner@gmail.com>
2022-07-05 14:03:59 -0300

    refatorar-scroll-suave

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 import ScrollSuave from './modules/scroll-suave';
+import ScrollAnima from './modules/scroll-anima';
 import initmenuMobile from './modules/menu-mobile';
-import initAnimacaoScroll from './modules/scroll-animacao';
 import initWritingEffect from './modules/writing-effect';
 import initFuncionamento from './modules/funcionamento';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
 initFuncionamento();
-initAnimacaoScroll();
 initmenuMobile();
 initWritingEffect();

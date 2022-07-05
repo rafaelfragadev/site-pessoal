32d3a29a7a3743e8bda4fa66a1bde1dcf06a8d34
Rafael Fraga <rafaelfragawebdesigner@gmail.com>
2022-07-05 19:05:40 -0300

    Formulario PHP

---
+++ js/modules/formulario.js
@@ -0,0 +1,28 @@
+export default function initFormulario(){
+  
+}
+
+const formulario = document.querySelector('form');
+
+function formularioEnviado(resposta){
+  if(resposta.ok) {
+    formulario.innerHTML = '<p class="font-1-p cor-p2" style="padding: 2rem; border-radius: 4px; background: var(--cor-0);"><span class="cor-p1 font-3-t">Mensagem enviada,</span> em breve entrarei em contato. Geralmente respondo em até 24 horas.</p>';
+  } else {
+    formulario.innerHTML = '<p class="font-1-p cor-4" style="padding: 2rem; border-radius: 4px; background: var(--cor-0);"><span class="cor-p1 font-3-t">Erro no envio,</span><br>você pode enviar diretamente para o nosso e-mail em: contato@rafaelfragawebdesigner.com.br</p>';
+  }
+}
+function enviarFormulario(event){
+  event.preventDefault();
+  const botao = document.querySelector('form button');
+  botao.disable = true;
+  botao.innerText = 'Enviando...';
+
+  const data = new FormData(formulario);
+  fetch('./enviar.php', {
+    method: 'POST',
+    body: data,
+  }).then(formularioEnviado);
+
+}
+
+formulario.addEventListener('submit', enviarFormulario);
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import ScrollAnima from './modules/scroll-anima';
 import initmenuMobile from './modules/menu-mobile';
 import initWritingEffect from './modules/writing-effect';
 import initFuncionamento from './modules/funcionamento';
+import initFormulario from './modules/formulario';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -10,6 +11,7 @@ scrollSuave.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
+initFormulario();
 initFuncionamento();
 initmenuMobile();
 initWritingEffect();
